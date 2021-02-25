15f2dc459550f166101b328df3eecb279cd833e7
Marcos-A <marcosalcocer@gmail.com>
2021-02-25 23:48:04 +0100

    Commented code added to index.php to print PHP error codes while debugging.

--- public/index.php
+++ public/index.php
@@ -54,6 +54,16 @@
         finfo_close($finfo);
     }
 
+    # Uncomment in case of PHP error to get the error message code while debugging:
+    # https://www.php.net/manual/en/features.file-upload.errors.php
+    /* if(array_key_exists('actaToUpload', $_FILES)){
+        if ($_FILES['actaToUpload']['error'] === UPLOAD_ERR_OK) {
+            echo 'upload was successful';
+        } else {
+            die("Upload failed with error code " . $_FILES['actaToUpload']['error']);
+        }
+    } */
+
     // Check if file already exists
     if (file_exists($target_file)) {
         echo "<br>El fitxer ja existeix. ";
@@ -111,110 +121,120 @@
     </form>
 <?php
 
-define("PYTHON_BUTLLETINS_SCRIPT", "ButlletinsSplitter.py");
-define("PYTHON_BUTLLETINS_SCRIPT_DIR", "../script/");
-define("RESULT_BUTLLETINS_ZIP_FILE", "butlletins.zip");
-define("RESULT_BUTLLETINS_ZIP_FILE_DIR", "../script/tmp/");
-define("TARGET_BUTLLETINS_DIR", "../uploads/");
+    define("PYTHON_BUTLLETINS_SCRIPT", "ButlletinsSplitter.py");
+    define("PYTHON_BUTLLETINS_SCRIPT_DIR", "../script/");
+    define("RESULT_BUTLLETINS_ZIP_FILE", "butlletins.zip");
+    define("RESULT_BUTLLETINS_ZIP_FILE_DIR", "../script/tmp/");
+    define("TARGET_BUTLLETINS_DIR", "../uploads/");
 
-$uploaded_file = $_FILES["butlletinsToUpload"]["tmp_name"];
-$fileType = "pdf";
-$target_file = TARGET_BUTLLETINS_DIR.time().".".$fileType;
-$uploadOk = true;
+    $uploaded_file = $_FILES["butlletinsToUpload"]["tmp_name"];
+    $fileType = "pdf";
+    $target_file = TARGET_BUTLLETINS_DIR.time().".".$fileType;
+    $uploadOk = true;
 
-// Check PDF file type
-if(isset($_POST["submitButlletins"])) {
-    $finfo = finfo_open(FILEINFO_MIME_TYPE);
-    
-    if(finfo_file($finfo, $uploaded_file) === 'application/pdf') {
-        $uploadOk = true;
-    } else {
+    // Check PDF file type
+    if(isset($_POST["submitButlletins"])) {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        
+        if(finfo_file($finfo, $uploaded_file) === 'application/pdf') {
+            $uploadOk = true;
+        } else {
+            $uploadOk = false;
+        }
+        finfo_close($finfo);
+    }
+
+    # Uncomment in case of PHP error to get the error message code while debugging:
+    # https://www.php.net/manual/en/features.file-upload.errors.php
+    /* if(array_key_exists('butlletinsToUpload', $_FILES)){
+        if ($_FILES['butlletinsToUpload']['error'] === UPLOAD_ERR_OK) {
+            echo 'upload was successful';
+        } else {
+            die("Upload failed with error code " . $_FILES['butlletinsToUpload']['error']);
+        }
+    } */
+
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "<br>El fitxer ja existeix. ";
         $uploadOk = false;
     }
-    finfo_close($finfo);
-}
 
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "<br>El fitxer ja existeix. ";
-    $uploadOk = false;
-}
-
-// Check file size
-if ($_FILES["butlletinsToUpload"]["size"] > 25000000) {
-    echo "<br>El fitxer pujat és massa gran. ";
-    $uploadOk = false;
-}
-
-// Check file is not empty
-if (isset($_POST["submitButlletins"]) && $_FILES["butlletinsToUpload"]["size"] == 0) {
-    echo "<br>El fitxer pujat està buit. ";
-    $uploadOk = false;
-}
-
-// Allow certain file formats
-if($fileType != "pdf" ) {
-    echo "<br>Només es poden pujar fitxers PDF. ";
-    $uploadOk = false;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if (!$uploadOk) {
-    echo "<br>No s'ha pogut pujar el seu fitxer. ";
-
-// if everything is ok, try to upload file
-} else {
-
-    if (move_uploaded_file($_FILES["butlletinsToUpload"]["tmp_name"], $target_file)) {
-
-        // Call the ButlletinsSplitter        
-        $ret_val = exec("python3 ".PYTHON_BUTLLETINS_SCRIPT_DIR.PYTHON_BUTLLETINS_SCRIPT." ".$target_file, $ret_val);
-        echo $ret_val;
-      
-        // Download zip file
-        $file = RESULT_BUTLLETINS_ZIP_FILE_DIR.RESULT_BUTLLETINS_ZIP_FILE;
-
-        if (headers_sent()) {
-            echo 'HTTP header already sent';
-        } else {
-            if (!is_file($file)) {
-                header($_SERVER['SERVER_PROTOCOL']." 404 Not Found");
-                echo "<br>No s'ha trobat el fitxer. ";
-            } else if (!is_readable($file)) {
-                header($_SERVER['SERVER_PROTOCOL']." 403 Forbidden");
-                echo "<br>El fitxer no es pot llegir. ";
-            } else {
-                header($_SERVER['SERVER_PROTOCOL']." 200 OK");
-                header("Content-type: application/zip"); 
-                header("Content-Transfer-Encoding: Binary");
-                header("Content-Length: ".filesize($file));
-                header("Content-Disposition: attachment; filename=\"".basename($file)."\"");
-
-                // Disable cache
-                header("Expires: " .gmdate('D, d M Y H:i:s') ." GMT");
-                header("Cache-control: private");
-                header("Pragma: private");
-
-                ob_end_clean();
-                readfile($file);
-
-                // Delete file
-                unlink($file);
-
-                // Clear temp zip file
-                system("rm -rf ../script/tmp/".RESULT_ZIP_FILE, $retval);
-
-                // Clear uploaded file
-                system("rm -rf ../uploads/*.pdf", $retval);
-
-                exit();
-            }
-        }
-
-    } else if(isset($_POST["submitButlletins"])) {
-        echo "<br>S'ha produït un error en pujar el seu fitxer. ";
+    // Check file size
+    if ($_FILES["butlletinsToUpload"]["size"] > 25000000) {
+        echo "<br>El fitxer pujat és massa gran. ";
+        $uploadOk = false;
     }
-}
+
+    // Check file is not empty
+    if (isset($_POST["submitButlletins"]) && $_FILES["butlletinsToUpload"]["size"] == 0) {
+        echo "<br>El fitxer pujat està buit. ";
+        $uploadOk = false;
+    }
+
+    // Allow certain file formats
+    if($fileType != "pdf" ) {
+        echo "<br>Només es poden pujar fitxers PDF. ";
+        $uploadOk = false;
+    }
+
+    // Check if $uploadOk is set to 0 by an error
+    if (!$uploadOk) {
+        echo "<br>No s'ha pogut pujar el seu fitxer. ";
+
+    // if everything is ok, try to upload file
+    } else {
+
+        if (move_uploaded_file($_FILES["butlletinsToUpload"]["tmp_name"], $target_file)) {
+
+            // Call the ButlletinsSplitter        
+            $ret_val = exec("python3 ".PYTHON_BUTLLETINS_SCRIPT_DIR.PYTHON_BUTLLETINS_SCRIPT." ".$target_file, $ret_val);
+            echo $ret_val;
+          
+            // Download zip file
+            $file = RESULT_BUTLLETINS_ZIP_FILE_DIR.RESULT_BUTLLETINS_ZIP_FILE;
+
+            if (headers_sent()) {
+                echo 'HTTP header already sent';
+            } else {
+                if (!is_file($file)) {
+                    header($_SERVER['SERVER_PROTOCOL']." 404 Not Found");
+                    echo "<br>No s'ha trobat el fitxer. ";
+                } else if (!is_readable($file)) {
+                    header($_SERVER['SERVER_PROTOCOL']." 403 Forbidden");
+                    echo "<br>El fitxer no es pot llegir. ";
+                } else {
+                    header($_SERVER['SERVER_PROTOCOL']." 200 OK");
+                    header("Content-type: application/zip"); 
+                    header("Content-Transfer-Encoding: Binary");
+                    header("Content-Length: ".filesize($file));
+                    header("Content-Disposition: attachment; filename=\"".basename($file)."\"");
+
+                    // Disable cache
+                    header("Expires: " .gmdate('D, d M Y H:i:s') ." GMT");
+                    header("Cache-control: private");
+                    header("Pragma: private");
+
+                    ob_end_clean();
+                    readfile($file);
+
+                    // Delete file
+                    unlink($file);
+
+                    // Clear temp zip file
+                    system("rm -rf ../script/tmp/".RESULT_ZIP_FILE, $retval);
+
+                    // Clear uploaded file
+                    system("rm -rf ../uploads/*.pdf", $retval);
+
+                    exit();
+                }
+            }
+
+        } else if(isset($_POST["submitButlletins"])) {
+            echo "<br>S'ha produït un error en pujar el seu fitxer. ";
+        }
+    }
 ?>
   </div>
 </div>
